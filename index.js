7164f26580f39c56dd67d82a5ec0a1a6e572d158
Rohan Agarwal <arohan2907@gmail.com>
2021-02-21 03:51:13 +0530

    The very first commitment.

---
+++ index.js
@@ -0,0 +1,17 @@
+const {token} = require('./config.json')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+        console.log(message.author);
+    }
+});
+
+client.login(token);
